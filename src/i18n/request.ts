4c770ce68d91c5d746bada6016499578d968374f
m6tm <maboadaniel.55@gmail.com>
2024-12-17 16:24:09 +0100

    feat: changement de l'alias minds par ai

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,3 +1,12 @@
+/**
+ * Proprietary and Confidential
+ * Copyright (c) 2024 Microminds Tech Ltd. All rights reserved.
+ *
+ * This software and its documentation are the exclusive property of
+ * Microminds Tech Ltd. No part of this software may be reproduced,
+ * distributed, or transmitted in any form or by any means without
+ * the prior written permission of Microminds Tech Ltd.
+ */
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
